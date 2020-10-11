612f4b5f096f4cd07032de9eb2e894c159caf35a
E <37807391+MrSeppukun@users.noreply.github.com>
2020-10-11 00:55:31 -0400

    Update Customer.java

--- src/main/java/sf/codingcompetition2020/structures/Customer.java
+++ src/main/java/sf/codingcompetition2020/structures/Customer.java
@@ -24,4 +24,82 @@ public class Customer {
 	private short yearsOfService;
 	private Integer vehiclesInsured;
 
+	public Customer(int customerId, String firstName, String lastName, int age, String area, int agentId, short agentRating, String primaryLanguage, List<Dependent> dependents, boolean homePolicy, boolean autoPolicy, boolean rentersPolicy, String totalMonthlyPremium, short yearsOfService, Integer vehiclesInsured) {
+		this.customerId = customerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.area = area;
+		this.agentId = agentId;
+		this.agentRating = agentRating;
+		this.primaryLanguage = primaryLanguage;
+		this.dependents = dependents;
+		this.homePolicy = homePolicy;
+		this.autoPolicy = autoPolicy;
+		this.rentersPolicy = rentersPolicy;
+		this.totalMonthlyPremium = totalMonthlyPremium;
+		this.yearsOfService = yearsOfService;
+		this.vehiclesInsured = vehiclesInsured;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public boolean isAutoPolicy() {
+		return autoPolicy;
+	}
+
+	public boolean isHomePolicy() {
+		return homePolicy;
+	}
+
+	public boolean isRentersPolicy() {
+		return rentersPolicy;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getAgentId() {
+		return agentId;
+	}
+
+	public Integer getVehiclesInsured() {
+		return vehiclesInsured;
+	}
+
+	public List<Dependent> getDependents() {
+		return dependents;
+	}
+
+	public short getAgentRating() {
+		return agentRating;
+	}
+
+	public short getYearsOfService() {
+		return yearsOfService;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getPrimaryLanguage() {
+		return primaryLanguage;
+	}
+
+	public String getTotalMonthlyPremium() {
+		return totalMonthlyPremium;
+	}
+
 }
