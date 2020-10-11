51cdfead9a9e86bf54a637abb1255597e50549b0
E <37807391+MrSeppukun@users.noreply.github.com>
2020-10-11 00:55:08 -0400

    Update Claim.java

--- src/main/java/sf/codingcompetition2020/structures/Claim.java
+++ src/main/java/sf/codingcompetition2020/structures/Claim.java
@@ -5,5 +5,29 @@ public class Claim {
 	private int customerId;
 	private boolean closed;
 	private int monthsOpen;
-	
+
+	public Claim(int claimId, int customerId, boolean closed, int monthsOpen) {
+		this.claimId = claimId;
+		this.customerId = customerId;
+		this.closed = closed;
+		this.monthsOpen = monthsOpen;
+	}
+
+	public int getClaimId() {
+		return claimId;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public int getMonthsOpen() {
+		return monthsOpen;
+	}
+
+	public boolean isClosed() {
+		return closed;
+	}
+
+
 }
