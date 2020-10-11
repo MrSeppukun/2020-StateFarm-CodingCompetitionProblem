da358ebf3ccc4213ddd0bf5f13e92f66f22f575f
E <37807391+MrSeppukun@users.noreply.github.com>
2020-10-11 00:56:24 -0400

    Update Vendor.java

--- src/main/java/sf/codingcompetition2020/structures/Vendor.java
+++ src/main/java/sf/codingcompetition2020/structures/Vendor.java
@@ -5,5 +5,27 @@ public class Vendor {
 	private String area;
 	private int vendorRating;
 	private boolean inScope;
-	
+
+	public Vendor(int vendorId, String area, int vendorRating, boolean inScope) {
+		this.vendorId = vendorId;
+		this.area = area;
+		this.vendorRating = vendorRating;
+		this.inScope = inScope;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public boolean isInScope() {
+		return inScope;
+	}
+
+	public int getVendorId() {
+		return vendorId;
+	}
+
+	public int getVendorRating() {
+		return vendorRating;
+	}
 }
